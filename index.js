b4f7d7d8c28ed0117178460fadded9adee533186
Jared Forsyth <jared@jaredforsyth.com>
2014-01-04 23:28:47 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 
-var express = require('express')
-  , cors = require('cors')
-  , app = express()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server)
-
-  , setup = require('./lib')
-  , config = require('./lib/config')
-
-app.use(cors())
-app.use(express.static(__dirname + '/public'))
-
-setup(io, app)
-
-server.listen(config.port, function () {
-  console.log('listening')
-})
+module.exports = require('./lib')
 
