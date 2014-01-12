a39e3ffec7e53aa89eb64b1f7c7b7c09ab2b12c2
Jared Forsyth <jared@jaredforsyth.com>
2014-01-11 20:49:59 -0700

    modify todo list etc.

--- client/index.js
+++ client/index.js
@@ -1,23 +1,11 @@
 
-var BaseManager = require('manager')
+var todos = require('../lib/todos')
+  , utils = require('./utils')
 
-function Manager(io) {
-	BaseManager.call(this, {
-		genId: function () {
-			throw new Error("You're not supposed to create people")
-		},
-		defaultNode: {
-			data: {},
-			rels: {},
-			more: {}
-		}
-	})
-	var that = this
-	io.on('person', function (id, person) {
-		that.set(id, person)
-	})
-  io.on('more_person', function (id, person, hastodos, numcrawled) {
-    that.set(id, person)
-  })
+module.exports = {
+  todos: {
+    map: todos,
+    titles: utils.getTodoTitles(todos)
+  }
 }
 
